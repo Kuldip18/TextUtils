42b1e0cdffae34f0dbeefd9a59f3fd3029a32336
Kuldip Barot <kuldipbarot2062004@gmail.com>
2024-09-16 13:47:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+ import TextForm from './components/TextForm';
+import About from './components/About';
+import Alert from './components/Alert';
+//import { type } from '@testing-library/user-event/dist/type';
+
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
+
 
 function App() {
+    const [mode,setMode]=useState('light');//Whether dark mode is enabled or not.
+    const[alert,setAlert]=useState(null);
+
+    const showAlert=(message,type)=>{
+      setAlert({
+        msg:message,
+        type:type
+      })
+      setTimeout(() => {
+        setAlert(null);
+      }, 1500);
+    }
+
+    const toggleMode=()=>{
+      if(mode === 'light'){
+        setMode('dark');
+        document.body.style.backgroundColor='#042743';
+        showAlert("Dark mode has been enabled","success");
+      }
+      else{
+        setMode('light');
+        document.body.style.backgroundColor='white';
+        showAlert("light mode has been enabled","success");
+      }
+    }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <>
+      <Navbar title="TextUtils" aboutText="About Text" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+       {/* <Navbar/> */}
+       <div className="container my=3"> 
+       {/* <Switch> */}
+       {/* <Routes> */}
+          {/* <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">*/}
+          {/* <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>  */}
+          <Routes>
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - Word counter,character counter,Remove extra spaces" mode={mode} />} />
+        </Routes> 
+           {/* </Route>
+          </Routes> */}
+        {/* </Switch>      */}  
+       {/* <About/> */}
+       </div>
+       
+       
+       
+    </>
+    // </Router>
   );
-}
+  }
+
 
 export default App;
